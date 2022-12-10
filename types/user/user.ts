091b7b8cd7f7cf0f6873b3408d4a651d612c1423
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2022-12-10 15:49:55 +0100

    fead: add RegisterUserResponse

--- types/user/user.ts
+++ types/user/user.ts
@@ -2,3 +2,14 @@ export enum UserRoles {
   ADMIN = 1,
   MEMBER,
 }
+
+export type RegisterUserResponse =
+  | {
+      id: string;
+      name: string;
+      email: string;
+    }
+  | {
+      statusCode: number;
+      message: string;
+    };
