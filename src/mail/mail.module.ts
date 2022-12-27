cc4e79cbd370b9a427c66ad95e3e83a890101b9e
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2022-12-27 15:14:50 +0100

    fead: add mail module

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,11 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { mailerConfigForSendGrid } from '../mailerConfigForSendGrid';
+
+@Module({
+  imports: [MailerModule.forRoot(mailerConfigForSendGrid)],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
