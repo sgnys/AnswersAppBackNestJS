f4271ba5ee25f3d438a09a345f6e4881b3839c68
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2022-12-28 22:58:41 +0100

    fead: create methods: sendRegisterLink and sendPasswordResetLink in mail.service.ts, add mail .hbs templates to registration link and password reset link

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,7 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { mailerConfigForSendGrid } from '../mailerConfigForSendGrid';
+import { mailerConfigForSendGrid } from '../../mailerConfigForSendGrid';
 
 @Module({
   imports: [MailerModule.forRoot(mailerConfigForSendGrid)],
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -4,4 +4,32 @@ import { MailerService } from '@nestjs-modules/mailer';
 @Injectable()
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
+
+  async sendRegisterLink(email: string, registerToken: string) {
+    const url = process.env.CLIENT_URL;
+    const mail = {
+      to: email,
+      subject: 'Answers APP registration link',
+      template: 'registration-link',
+      context: {
+        registerToken,
+        url,
+      },
+    };
+    await this.mailerService.sendMail(mail);
+  }
+
+  async sendPasswordResetLink(email: string, resetLinkToken: string) {
+    const url = process.env.CLIENT_URL;
+    const mail = {
+      to: email,
+      subject: 'Answers APP link to reset your password',
+      template: 'password-reset-link',
+      context: {
+        resetLinkToken,
+        url,
+      },
+    };
+    await this.mailerService.sendMail(mail);
+  }
 }
