ad66cd80330bafa50febbb14990b87d292391368
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2023-01-07 11:56:12 +0100

    fead: add basic method 'login' in auth controller with LocalAuthGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from 'src/guards/local-auth.guard';
+import { AuthService } from './auth.service';
+import { Request } from 'express';
 
 @Controller('api/auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Req() req: Request): Promise<any> {
+    console.log(req.user);
+    return req.user;
+  }
+}
