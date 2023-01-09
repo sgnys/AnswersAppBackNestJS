fc8af9122bc21400c82e54de1e4b0da01a77548b
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2023-01-09 18:03:09 +0100

    fead: add method login() in auth.service.ts. This method create access token using two additional private method: createToken() and generateToken()

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,50 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Response } from 'express';
+import { sign } from 'jsonwebtoken';
+import { v4 as uuid } from 'uuid';
 import { UserService } from '../user/user.service';
 import { hashPwd } from '../utils/hash-pwd';
+import { AuthLoginDto } from './dto/auth-login.dto';
+import { UserEntity } from '../user/user.entity';
+import { sanitizeUser } from '../utils/sanitize-user';
+
+export interface JwtPayload {
+  tokenId: string;
+}
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
+
+  private createToken(currentTokenId: string): {
+    accessToken: string;
+    expiresIn: number;
+  } {
+    const payload: JwtPayload = { tokenId: currentTokenId };
+    const expiresIn = 60 * 60 * 12;
+    const accessToken = sign(payload, process.env.JWT_SECRET, { expiresIn });
+    return {
+      accessToken,
+      expiresIn,
+    };
+  }
+
+  private async generateToken(user: UserEntity): Promise<string> {
+    let token;
+    let userWithThisToken = null;
+    do {
+      token = uuid();
+      userWithThisToken = await UserEntity.findOne({
+        where: { currentTokenId: token },
+      });
+    } while (!!userWithThisToken);
+    user.currentTokenId = token;
+    await user.save();
+
+    return token;
+  }
 
   async validateUserCreds(email: string, password: string): Promise<any> {
     const user = await this.userService.getUserByEmail(email);
@@ -20,5 +58,34 @@ export class AuthService {
       throw new UnauthorizedException();
     }
     return user;
+  }
+
+  async login(loginDto: AuthLoginDto, res: Response): Promise<any> {
+    console.log(loginDto);
+    try {
+      const user = await UserEntity.findOne({
+        where: {
+          email: loginDto.email,
+          password: hashPwd(loginDto.password),
+        },
+      });
+
+      if (!user) {
+        throw new BadRequestException('Invalid login data!');
+      }
+
+      const token = this.createToken(await this.generateToken(user));
+
+      return res
+        .cookie('jwt', token.accessToken, {
+          secure: false,
+          domain: 'localhost',
+          httpOnly: true,
+        })
+        .json(sanitizeUser(user));
+    } catch (e) {
+      console.log('Failed to log in', e);
+      throw new UnauthorizedException('Failed to log in');
+    }
   }
 }
