f8778830d60e8e54a04536eab7049046f4fff442
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2023-01-09 18:35:41 +0100

    fead: create example path to JwtAuthGuard tests

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 import { LocalAuthGuard } from 'src/guards/local-auth.guard';
 import { AuthService } from './auth.service';
 import { AuthLoginDto } from './dto/auth-login.dto';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { sanitizeUser } from '../utils/sanitize-user';
 
 @Controller('api/auth')
 export class AuthController {
@@ -18,5 +28,12 @@ export class AuthController {
     console.log(req.user);
     console.log(loginDto);
     return this.authService.login(loginDto, res);
+  }
+
+  //TODO delete this method after tests
+  @UseGuards(JwtAuthGuard)
+  @Get('testPathForJwtAuthGuard')
+  async testPathForJwtAuthGuard(@Req() req: Request): Promise<any> {
+    return req.user;
   }
 }
