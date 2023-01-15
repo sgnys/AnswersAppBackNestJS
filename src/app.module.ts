eb3a32186321dd5161d04121c67950106e7d3e3e
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2023-01-15 11:50:56 +0100

    fead: create basic answer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { UserModule } from './user/user.module';
 import { UserEntity } from './user/user.entity';
 import { MailModule } from './mail/mail.module';
 import { AuthModule } from './auth/auth.module';
+import { AnswerModule } from './answer/answer.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { AuthModule } from './auth/auth.module';
     UserModule,
     MailModule,
     AuthModule,
+    AnswerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
