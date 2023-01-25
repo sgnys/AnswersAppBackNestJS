18b23e9bab28e78327264da63add6ff4eec4fccc
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2023-01-25 14:05:59 +0100

    fead: add all endpoints in answer-template module without relations and without role guard

--- src/answer-template/answer-template.service.ts
+++ src/answer-template/answer-template.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AnswerTemplateEntity } from './answer-template.entity';
+import { AnswerTemplateUpdateDto } from './dto/answer-template-update.dto';
+import { DEFAULT_TEMPLATES } from '../utils/constants';
 
 @Injectable()
-export class AnswerTemplateService {}
+export class AnswerTemplateService {
+  async getAll(): Promise<AnswerTemplateEntity[]> {
+    return AnswerTemplateEntity.find();
+  }
+
+  async getTemplateById(id: string): Promise<AnswerTemplateEntity> {
+    return AnswerTemplateEntity.findOne({
+      where: { id },
+    });
+  }
+
+  async updateTemplate(
+    id: string,
+    data: AnswerTemplateUpdateDto,
+  ): Promise<AnswerTemplateEntity> {
+    const template = await this.getTemplateById(id);
+    console.log(template);
+
+    if (template === null) {
+      throw new BadRequestException(
+        'Cannot edit an template that does not exist',
+      );
+    }
+
+    console.log(data);
+
+    if (
+      template.firstParagraph === data.firstParagraph &&
+      template.lastParagraph === data.lastParagraph
+    ) {
+      throw new BadRequestException(
+        'You have not made any changes to the template, the modification has not been uploaded to the server',
+      );
+    }
+
+    template.firstParagraph = data.firstParagraph;
+    template.lastParagraph = data.lastParagraph;
+
+    await template.save();
+
+    return template;
+  }
+
+  async createDefaultTemplatesForRegisterUser() {
+    const customerTemplate = await new AnswerTemplateEntity();
+
+    customerTemplate.name = DEFAULT_TEMPLATES.CUSTOMER.NAME;
+
+    customerTemplate.firstParagraph =
+      DEFAULT_TEMPLATES.CUSTOMER.FIRST_PARAGRAPH;
+
+    customerTemplate.lastParagraph = DEFAULT_TEMPLATES.CUSTOMER.LAST_PARAGRAPH;
+
+    await customerTemplate.save();
+
+    const consultantTemplate = await new AnswerTemplateEntity();
+
+    consultantTemplate.name = DEFAULT_TEMPLATES.CONSULTANT.NAME;
+
+    consultantTemplate.firstParagraph =
+      DEFAULT_TEMPLATES.CONSULTANT.FIRST_PARAGRAPH;
+
+    consultantTemplate.lastParagraph =
+      DEFAULT_TEMPLATES.CONSULTANT.LAST_PARAGRAPH;
+
+    await consultantTemplate.save();
+  }
+}
