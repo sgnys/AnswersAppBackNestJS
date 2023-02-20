ecd1526e77fb622704c926179dcb79cc4b89cf2d
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2023-02-20 18:30:51 +0100

    fead: create AuthLoginResDto with decorator @ApiProperty()

---
+++ src/auth/dto/auth-login-res.dto.ts
@@ -0,0 +1,31 @@
+import { IsEmail, IsEnum, IsNotEmpty } from 'class-validator';
+import { UserRoles } from 'types';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AuthLoginResDto {
+  @ApiProperty({
+    description: 'User id',
+    example: '100',
+  })
+  @IsNotEmpty()
+  id: string;
+
+  @ApiProperty({ enum: UserRoles })
+  @IsEnum(UserRoles)
+  role: UserRoles;
+
+  @ApiProperty({
+    description: 'Email address of the user',
+    example: 'user@example.com',
+  })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description: 'The name of the user',
+    example: 'Sławek',
+  })
+  @IsNotEmpty()
+  name: string;
+}
