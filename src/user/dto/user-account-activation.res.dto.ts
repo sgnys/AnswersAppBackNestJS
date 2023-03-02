c29d3625ab5bb38acc964ae51a22e83eb2a3a045
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2023-03-02 22:52:28 +0100

    fead: add UserAccountActivationResDto

---
+++ src/user/dto/user-account-activation.res.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsEnum, IsNotEmpty } from 'class-validator';
+import { UserRoles } from 'types';
+
+export class UserAccountActivationResDto {
+  @ApiProperty({
+    description: 'User id',
+    example: '100',
+  })
+  @IsNotEmpty()
+  id: string;
+
+  @ApiProperty({
+    description: 'UserRoles: 1-ADMIN, 2-MEMBER',
+    example: 2,
+    enum: UserRoles,
+  })
+  @IsEnum(UserRoles)
+  role: UserRoles;
+
+  @ApiProperty({
+    description: 'Email address of the user',
+    example: 'user@example.com',
+  })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description: 'The name of the user',
+    example: 'Sławek',
+  })
+  @IsNotEmpty()
+  name: string;
+}
